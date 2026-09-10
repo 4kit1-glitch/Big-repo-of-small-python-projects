bd715dbad06e6289de251e04226f00fdb1a711b9
Kengah Ireneaus <4jrkit1@gmail.com>
2026-09-10 18:29:57 +0100

    implemented strip with regex

--- medium/regexstrip.py
+++ medium/regexstrip.py
@@ -1,8 +1,20 @@
 import re
 
-def restrip(text: str, pattern= None):
+def perform_strip(text: str, pattern) -> str:
     if pattern is None:
-        pat = re.compile(r'(^\s*)?(\s*$)?')
-    else:
-        pat = re.compile(rf'^(?:{re.escape(pattern)})+|(?:{re.escape(pattern)}+$)')
-    return pat.sub("", text)
+        strip_pattern = re.compile(r'^\s+|\s+$')
+        return strip_pattern.sub("", text)
+    charset = re.escape(pattern)
+    strip_pattern = re.compile(rf'^[{charset}]+|[{charset}]+$')
+
+    return strip_pattern.sub("", text)
+
+def restrip(text: str, pattern= None):
+    return perform_strip(text, pattern)
+
+
+
+
+print(restrip("-----------------.............hello...........-----------", ".-"))
+
+print(restrip("cabacbabababababhellobat", "ab"))
